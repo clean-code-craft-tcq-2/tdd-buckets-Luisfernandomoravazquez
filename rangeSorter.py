002a69ae367dd1b1d44f21fc5f8f3d367e25511e
mur1ga <mur1ga@rbcm-gerrit.de.bosch.com>
2022-03-10 19:07:04 -0600

    rangeSorter script and testing

---
+++ rangeSorter.py
@@ -0,0 +1,34 @@
+class range():
+    def __init__(self, minValue, maxValue):
+        self.minValue = minValue
+        self.maxValue = maxValue
+        self.counter = 0
+    
+    def is_in_range_num(self, value):
+        if( value >= self.minValue and value <= self.maxValue):
+            return 1
+        else:
+            return 0
+
+    def analyze_list(self, list):
+        # self.counter = 0
+        for element in list:
+            self.counter += self.is_in_range_num(element)
+
+    def get_comma_separated_line(self):
+        return "{}-{},{}".format(self.minValue,self.maxValue,self.counter)
+
+# def string_to_csv_file(string):
+#     text_file = open("output.csv", "w")
+#     n = text_file.write(string)
+#     text_file.close()
+
+ranges = [range(3,5),range(10,12)]
+
+def sort_in_ranges(toSort_lst):
+    csvText = ""
+    for range in ranges:
+        range.analyze_list(toSort_lst)
+        csvText += range.get_comma_separated_line()+"\n"
+    return csvText
+    # string_to_csv_file(csvText)
---
+++ rangeSorter.test.py
@@ -0,0 +1,30 @@
+import unittest
+import rangeSorter
+
+class TypewiseTest(unittest.TestCase):
+
+    def smallest_possible_failing_test(self):
+        self.assertNotEqual(rangeSorter.sort_in_ranges([4,5]),"4-5, 2")
+
+    def smallest_possible_passing_test(self):
+        self.assertEqual(rangeSorter.sort_in_ranges([4,5]),"3-5,2\n10-12,0\n")
+
+    def not_int_range_test(self): # just for 100% coverage
+        self.assertEqual(rangeSorter.sort_in_ranges([0]),"3-5,0\n10-12,0\n")
+
+    # def test_range_class_methods(self): # Not necesary for 100% coverage 
+    #     # Create dummy object to test
+    #     rangeExampleObject = rangeSorter.range(-2,8)
+    #     # Test is_in_range_num() method with a number NOT in the range
+    #     self.assertEqual(rangeExampleObject.is_in_range_num(-3),0)
+    #     # Test is_in_range_num() method with a number in range
+    #     self.assertEqual(rangeExampleObject.is_in_range_num(8),1)
+    #     # Test get_comma_separated_line() method with a number in range and another not in range
+    #     rangeExampleObject.analyze_list([5,23])
+    #     self.assertEqual(rangeExampleObject.get_comma_separated_line(),"-2-8,1")
+
+    # def unnecesary_extra_test(self):
+    #     self.assertEqual(rangeSorter.sort_in_ranges([4,16,-1,3,9.-2,8]),"3-5,1\n10-12,0\n")
+
+if __name__ == '__main__':
+    unittest.main()
